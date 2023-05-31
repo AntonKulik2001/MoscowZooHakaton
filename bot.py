9554114b8c1842ad689f46f337031cec7a11101b
Antonkulik2001 <antonkulik201@gmail.com>
2023-05-31 19:31:15 +0300

    Create Project

---
+++ bot.py
@@ -0,0 +1,13 @@
+import telebot
+from config import TOKEN
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['help'])
+def help(message: telebot.types.Message):
+    text = 'Викторина созданная для Хакатона Московского Зоопарка! Для того чтобы начать введите "/start".'
+
+    bot.reply_to(message, text)
+
+
+bot.polling(none_stop=True)
